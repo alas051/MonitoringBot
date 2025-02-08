9dd6ffbdb32c193feec55783cb752d52a5240463
alas051 <81924679+alas051@users.noreply.github.com>
2025-02-08 11:10:41 +0330

    Update setup.sh
    add change ip

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # دریافت مسیر دایرکتوری جاری که اسکریپت در آن قرار دارد
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-BOT_URL="http://80.244.11.136/monitoringBot.py"
+BOT_URL="http://77.83.203.147/monitoringBot.py"
 BOT_FILE="$SCRIPT_DIR/monitoringBot.py"
 
 # نصب وابستگی‌های موردنیاز
@@ -24,11 +24,19 @@ echo "✅ Download successful."
 # دریافت `TOKEN` و `ADMIN_USER_ID` از کاربر
 read -p "Enter your Telegram BOT TOKEN: " TOKEN
 read -p "Enter your Telegram ADMIN USER ID: " ADMIN_USER_ID
+read -p "Enter your server IP address: " SERVER_IP
+
+# بررسی معتبر بودن فرمت IP
+if [[ ! $SERVER_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "❌ Invalid IP format. Please enter a valid IP address."
+    exit 1
+fi
 
 # بروزرسانی مقادیر `TOKEN` و `ADMIN_USER_ID` در فایل `monitoringBot.py`
-echo "⚙️ Updating the script with the provided TOKEN and ADMIN_USER_ID..."
+echo "⚙️ Updating the script with the provided TOKEN, ADMIN_USER_ID, and SERVER_IP..."
 sed -i "s|TOKEN = \".*\"|TOKEN = \"$TOKEN\"|" "$BOT_FILE"
 sed -i "s|ADMIN_USER_ID = [0-9]*|ADMIN_USER_ID = $ADMIN_USER_ID|" "$BOT_FILE"
+sed -i "s|77\.83\.203\.147|$SERVER_IP|g" "$BOT_FILE"
 
 # بررسی موفقیت جایگزینی
 if [ $? -ne 0 ]; then
